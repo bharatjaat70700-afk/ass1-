57e356cd05f5dfcecb74e94bc019347826ee0f01
bharatjaat70700-afk <bharatjaat70700@gmail.com>
2026-02-12 10:50:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const eventTitle=Document.getElementById("eventTitle");
+const eventDate=Document.getElementById("eventDate");
+const eventCategory=Document.getElementById("eventCategory");
+const eventDescription=Document.getElementById("eventDescription");
+const eventForm=Document.getElementById("eventForm");
+const eventContainer=Document.getElementById("eventContainer");
+const clearAllBtn=Document.getElementById("clearAllBtn");
+const addSampleBtn=Document.getElementById("addSampleBtn");
+
+
+const sampleEvents=[
+    {
+        title:"Birthday Party",
+        date:"2024-07-15",
+        category:"Personal",
+        description:"Celebrate with friends and family!"
+    },
+     {
+        title:"Birthday Party",
+        date:"2024-07-15",
+        category:"Personal",
+        description:"Celebrate with friends and family!"
+    }
+    
+    
+]
